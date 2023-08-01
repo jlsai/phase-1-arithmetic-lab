4027435b971d1f56effcecc15e6f28a5ba1fbfcf
jlsai <jon.lea@outlook.com>
2023-08-01 03:10:53 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31
+const num2 = 2
+const num3 = 4;
+const num4 = 16;
 
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random() * 10)
+const mod = num3 % num4;
+const max = Math.max(2, 6, 20, 12);
 // Write your code here
